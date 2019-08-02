77df9f0b198b58fb6fb021fbf3ae1d163842df65
edwin <edwin.lzh@gmail.com>
2019-08-02 12:10:48 +0800

    process start && stop

--- process.go
+++ process.go
@@ -1,16 +1,36 @@
 package supervisorxmlrpc
 
 const (
-	MethodGetProcessInfo    string = "getProcessInfo"
-	MethodGetAllProcessInfo string = "getAllProcessInfo"
+	MethodGetProcessInfo      string = "getProcessInfo"
+	MethodGetAllProcessInfo   string = "getAllProcessInfo"
+	MethodStartProcess        string = "startProcess"
+	MethodStartAllProcess     string = "startAllProcesses"
+	MethodStartProcessGroup   string = "startProcessGroup"
+	MethodStopProcess         string = "stopProcess"
+	MethodStopAllProcess      string = "stopAllProcesses"
+	MethodStopProcessGroup    string = "stopProcessGroup"
+	MethodSignalProcess       string = "signalProcess"
+	MethodSignalAllProcess    string = "signalAllProcesses"
+	MethodSignalProcessGroup  string = "signalProcessGroup"
+	MethodSendProcessStdin    string = "sendProcessStdin"
+	MethodSendRemoteCommEvent string = "sendRemoteCommEvent"
+	MethodReloadConfig        string = "reloadConfig"
+	MethodAddProcessGroup     string = "addProcessGroup"
+	MethodRemoveProcessGroup  string = "removeProcessGroup"
 )
 
 type ProcessApiInterface interface {
-	GetProcessInfo(string) (GetProcessInfoReturn, error)
-	GetAllProcessInfo() ([]GetProcessInfoReturn, error)
+	GetProcessInfo(string) (ProcessInfoReturn, error)
+	GetAllProcessInfo() ([]ProcessInfoReturn, error)
+	StartProcess(string, bool) (bool, error)
+	StartAllProcess(bool) ([]ProcessInfoReturn, error)
+	StartProcessGroup(string, bool) ([]ProcessInfoReturn, error)
+	StopProcess(string, bool) (bool, error)
+	StopAllProcesses(bool) ([]ProcessInfoReturn, error)
+	StopProcessGroup(string, bool) ([]ProcessInfoReturn, error)
 }
 
-type GetProcessInfoReturn struct {
+type ProcessInfoReturn struct {
 	Name          string `xmlrpc:"name"`
 	Group         string `xmlrpc:"group"`
 	Description   string `xmlrpc:"description"`
@@ -27,21 +47,77 @@ type GetProcessInfoReturn struct {
 	Pid           int    `xmlrpc:"pid"`
 }
 
-func (c *Client) GetProcessInfo(name string) (ret *GetProcessInfoReturn, err error) {
-	ret = new(GetProcessInfoReturn)
+func (c *Client) GetProcessInfo(name string) (ret *ProcessInfoReturn, err error) {
+	ret = new(ProcessInfoReturn)
 	err = c.CallStruct(GetSupervisorMethod(MethodGetProcessInfo), ret, name)
 	return
 }
 
-func (c *Client) GetAllProcessInfo() (ret []GetProcessInfoReturn, err error) {
+func (c *Client) GetAllProcessInfo() (ret []ProcessInfoReturn, err error) {
 	err = c.CallStruct(GetSupervisorMethod(MethodGetAllProcessInfo), &ret, nil...)
 	return
 }
 
-func GetProcessInfo(client *Client, name string) (*GetProcessInfoReturn, error) {
+func (c *Client) StartProcess(name string, wait bool) (bool, error) {
+	return c.CallBool(GetSupervisorMethod(MethodStartProcess), name, wait)
+}
+
+func (c *Client) StartAllProcess(wait bool) (ret []ProcessInfoReturn, err error) {
+	ret = make([]ProcessInfoReturn, 0)
+	err = c.CallStruct(GetSupervisorMethod(MethodStartAllProcess), &ret, wait)
+	return
+}
+
+func (c *Client) StartProcessGroup(group string, wait bool) (ret []ProcessInfoReturn, err error) {
+	ret = make([]ProcessInfoReturn, 0)
+	err = c.CallStruct(GetSupervisorMethod(MethodStartProcessGroup), &ret, group, wait)
+	return
+}
+
+func (c *Client) StopProcess(name string, wait bool) (bool, error) {
+	return c.CallBool(GetSupervisorMethod(MethodStopProcess), name, wait)
+}
+
+func (c *Client) StopAllProcess(wait bool) (ret []ProcessInfoReturn, err error) {
+	ret = make([]ProcessInfoReturn, 0)
+	err = c.CallStruct(GetSupervisorMethod(MethodStopAllProcess), &ret, wait)
+	return
+}
+
+func (c *Client) StopProcessGroup(group string, wait bool) (ret []ProcessInfoReturn, err error) {
+	ret = make([]ProcessInfoReturn, 0)
+	err = c.CallStruct(GetSupervisorMethod(MethodStopProcessGroup), &ret, group, wait)
+	return
+}
+
+func GetProcessInfo(client *Client, name string) (*ProcessInfoReturn, error) {
 	return client.GetProcessInfo(name)
 }
 
-func GetAllProcessInfo(client *Client) ([]GetProcessInfoReturn, error) {
+func GetAllProcessInfo(client *Client) ([]ProcessInfoReturn, error) {
 	return client.GetAllProcessInfo()
+}
+
+func StartProcess(client *Client, name string, wait bool) (bool, error) {
+	return client.StartProcess(name, wait)
+}
+
+func StartAllProcess(client *Client, wait bool) ([]ProcessInfoReturn, error) {
+	return client.StartAllProcess(wait)
+}
+
+func StartProcessGroup(client *Client, group string, wait bool) ([]ProcessInfoReturn, error) {
+	return client.StartProcessGroup(group, wait)
+}
+
+func StopProcess(client *Client, name string, wait bool) (bool, error) {
+	return client.StopProcess(name, wait)
+}
+
+func StopAllProcess(client *Client, wait bool) ([]ProcessInfoReturn, error) {
+	return client.StopAllProcess(wait)
+}
+
+func StopProcessGroup(client *Client, group string, wait bool) ([]ProcessInfoReturn, error) {
+	return client.StopProcessGroup(group, wait)
 }
--- process_test.go
+++ process_test.go
@@ -12,16 +12,16 @@ func TestGetProcessInfo(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    *GetProcessInfoReturn
+		want    *ProcessInfoReturn
 		wantErr bool
 	}{
 		{
 			name: "getProcessInfo",
 			args: args{
 				client: newConnect(t, testServer),
-				name:   "single",
+				name:   "demo:single",
 			},
-			want: &GetProcessInfoReturn{
+			want: &ProcessInfoReturn{
 				Name: "single",
 			},
 		},
@@ -68,6 +68,218 @@ func TestGetAllProcessInfo(t *testing.T) {
 			}
 			if len(got) < tt.want {
 				t.Errorf("GetAllProcessInfo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStopProcess(t *testing.T) {
+	type args struct {
+		client *Client
+		name   string
+		wait   bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "stopProcess",
+			args: args{
+				client: newConnect(t, testServer),
+				name:   "demo:single",
+				wait:   true,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StopProcess(tt.args.client, tt.args.name, tt.args.wait)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StopProcess() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("StopProcess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStartProcess(t *testing.T) {
+	type args struct {
+		client *Client
+		name   string
+		wait   bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "startProcess",
+			args: args{
+				client: newConnect(t, testServer),
+				name:   "demo:single",
+				wait:   true,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StartProcess(tt.args.client, tt.args.name, tt.args.wait)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StartProcess() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("StartProcess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStopAllProcess(t *testing.T) {
+	type args struct {
+		client *Client
+		wait   bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "stopAllProcess",
+			args: args{
+				client: newConnect(t, testServer),
+				wait:   true,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StopAllProcess(tt.args.client, tt.args.wait)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StopAllProcess() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) < tt.want {
+				t.Errorf("StopAllProcess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStartAllProcess(t *testing.T) {
+	type args struct {
+		client *Client
+		wait   bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "startAllProcess",
+			args: args{
+				client: newConnect(t, testServer),
+				wait:   true,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StartAllProcess(tt.args.client, tt.args.wait)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StartAllProcess() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) < tt.want {
+				t.Errorf("StartAllProcess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStopProcessGroup(t *testing.T) {
+	type args struct {
+		client *Client
+		group  string
+		wait   bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "stopProcessGroup",
+			args: args{
+				client: newConnect(t, testServer),
+				group:  "demo",
+				wait:   true,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StopProcessGroup(tt.args.client, tt.args.group, tt.args.wait)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StopProcessGroup() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) < tt.want {
+				t.Errorf("StopProcessGroup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStartProcessGroup(t *testing.T) {
+	type args struct {
+		client *Client
+		group  string
+		wait   bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "startProcessGroup",
+			args: args{
+				client: newConnect(t, testServer),
+				group:  "demo",
+				wait:   true,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StartProcessGroup(tt.args.client, tt.args.group, tt.args.wait)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StartProcessGroup() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) < tt.want {
+				t.Errorf("StartProcessGroup() = %v, want %v", got, tt.want)
 			}
 		})
 	}
